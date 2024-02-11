847517f720c36153490c255d22feb7a2f108621c
Antoniomary <amanso.totti@yahoo.com>
2024-02-10 17:45:52 -0800

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+    Test for console
+"""
+import unittest
+from models.console import HBNNCommand
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """A class that tests the console"""
+    pass
