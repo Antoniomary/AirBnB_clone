48352b06f57499704fcd26bbe130a5ccd49a9154
Antoniomary <amanso.totti@yahoo.com>
2024-02-11 19:31:56 -0800

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -23,6 +23,99 @@ EOF  all  count  create  destroy  help  quit  show  update
 """
         self.assertEqual(s, f.getvalue())
 
+    def test_help_EOF(self):
+        """Tests the help for EOF"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+        s = """EOF (CTRL + D) exits the program
+        \n"""
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_all(self):
+        """Tests the help for all"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+        s = """Prints all string representation of all instances.
+
+        Usage: all
+                   prints the entire instances of the different classes
+               all <class name>
+                   prints instances of only <class name>
+               <class name>.all()
+                   also prints instances of only <class name>
+        \n"""
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_count(self):
+        """Tests the help for count"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help count")
+        s = """Counts the number of instances of a class
+
+        Usage: <class name>.count()
+        \n"""
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_create(self):
+        """Tests the help for create"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+        s = "Creates a class instance, saves it to a JSON file"
+        s += """ and prints the id
+
+        Usage: create <class name>
+        \n"""
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_destroy(self):
+        """Tests the help for destroy"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help destroy")
+        s = """Deletes an instance and saves the change into a JSON file.
+
+        Usage: destroy <class name> <id>
+        \n"""
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_help(self):
+        """Tests the help for help"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help help")
+        s = 'List available commands with "help"'
+        s += ''' or detailed help with "help cmd".\n'''
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_quit(self):
+        """Tests the help for quit"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+        s = """Quit command to exit the program
+        \n"""
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_show(self):
+        """Tests the help for show"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help show")
+        s = """Prints the string representation of an instance
+
+        Usage: show <class name> <instance id>
+        \n"""
+        self.assertEqual(s, f.getvalue())
+
+    def test_help_update(self):
+        """Tests the help for update"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help update")
+        s = "Updates/adds instance attribute and"
+        s += ''' saves the change to a JSON file
+
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+               <class name>.update(<id>, <attribute name>, <attribute value>)
+               <class name>.update(<id>, <dictionary representation>).
+        \n'''
+        self.assertEqual(s, f.getvalue())
+
 
 if __name__ == "__main__":
     unittest.main()
