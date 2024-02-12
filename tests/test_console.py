64c07039d5bdac6dc4245271e38b09aabfc1a5f3
Antoniomary <amanso.totti@yahoo.com>
2024-02-11 18:34:37 -0800

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,8 +4,25 @@
 """
 import unittest
 from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
 
 
 class TestHBNBCommand(unittest.TestCase):
     """A class that tests the console"""
-    pass
+
+    def test_help(self):
+        """Tests the help command"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help")
+        s = """
+Documented commands (type help <topic>):
+========================================
+EOF  all  count  create  destroy  help  quit  show  update
+
+"""
+        self.assertEqual(s, f.getvalue())
+
+
+if __name__ == "__main__":
+    unittest.main()
